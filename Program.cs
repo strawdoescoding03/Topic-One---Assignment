084a0fb00728dcacce8421873f578ce6edc974b3
Sarah Tiffany <362575441@GoTVDSB.ca>
2025-02-04 14:15:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_One___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
