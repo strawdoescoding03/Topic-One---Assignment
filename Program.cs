7596c9db0545449478f786cb14e0b20866a16a88
Sarah Tiffany <362575441@GoTVDSB.ca>
2025-02-04 14:41:59 -0500

    RAAAAAAH It is completed!

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, User! I am Sarah, Here to share with you my innital art!");
+            Console.WriteLine();
+            //Question One: The Importance of Comments
+
+            Console.WriteLine("Comments are used in code to organize, lable and source code");
+            Console.WriteLine("within the program. When coding, comments can be helpful for");
+            Console.WriteLine("labeling the function on the code or organizing the parts.");
+            Console.WriteLine("If code does not belong to you, you can use a comment to source the creator!");
+            Console.WriteLine();
+            
+            Console.WriteLine("  S S S        A      R R R R         A      H       H    T T T T T");
+            Console.WriteLine("S       S    A   A    R       R     A   A    H       H        T");
+            Console.WriteLine("S          A       A  R       R   A       A  H       H        T");
+            Console.WriteLine("  S S S    A A A A A  R R R R     A A A A A  H H H H H        T");
+            Console.WriteLine("        S  A       A  R   R       A       A  H       H        T");
+            Console.WriteLine("S       S  A       A  R     R     A       A  H       H  ..    T");
+            Console.WriteLine("  S S S    A       A  R       R   A       A  H       H  ..    T");
         }
     }
 }
